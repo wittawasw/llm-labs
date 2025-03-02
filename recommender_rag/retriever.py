0b9d47f559489f4c4a4cd04899379317c690b9b4
wittawasw <start@wittawasw.com>
2025-03-03 04:14:52 +0700

    trt bge-m3 finetuned

--- recommender_rag/retriever.py
+++ recommender_rag/retriever.py
@@ -3,8 +3,10 @@ import sqlite3
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+# embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
 # embedding_model_name = "sentence-transformers/multi-qa-MiniLM-L6-cos-v1"
+embedding_model_name = "alifaheem94/bge-m3_medical_ur_ru"
+
 embedding_model = SentenceTransformer(embedding_model_name)
 
 def get_embedding(text):
